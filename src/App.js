dce974c0c1da6f522302e4bc4499196fca50bc5a
Ilham <ilham.sykes@hotmail.com>
2020-12-13 20:29:05 +0700

    add icons on links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
 import './App.css';
 import logo from './images/logo.png'
-import {FacebookSquare, InstagramAlt, Instagram} from '@styled-icons/boxicons-logos/'
+import {FacebookSquare, InstagramAlt} from '@styled-icons/boxicons-logos/'
 import {Code} from '@styled-icons/entypo/'
+import {ShoppingCart} from '@styled-icons/foundation/'
+import {ShoppingBag} from '@styled-icons/material/'
+import {WhatsappSquare} from '@styled-icons/boxicons-logos/'
 
 function App() {
   return (
@@ -22,18 +25,21 @@ function App() {
 
             <div className="Link">
               <div className="Link-container">
+                <ShoppingCart size='2em' color="lightblue"/>
                 <a href="https://sorudeshop.com/">Website (Fast Order)</a>
               </div>
             </div>
 
             <div className="Link">
               <div className="Link-container">
+              <WhatsappSquare size='2em' color='green'/>
                 <a href="https://wa.me/628998140989">Whatsapp</a>
               </div>                
             </div>
               
             <div className="Link">
               <div className="Link-container">
+                <ShoppingBag size='2em' color='darkorange'/>
                 <a href="https://wa.me/628998140989">Shopee</a>
               </div>                
             </div>
